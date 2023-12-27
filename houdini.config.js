992fa27e5f2e79cbb3e53fc50aed21009a401c9f
Tiến Senior <good66612@gmail.com>
2023-12-27 13:27:34 +0700

    resolved: init project

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://spacex-production.up.railway.app/"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$houdini';
+
+	export let data: PageData;
+
+	$: ({ Launches } = data);
+</script>
+
+<button color="alternative" on:click={() => data.Launches.loadNextPage()}>Load more</button>
+
+{#if $Launches.data?.launches}
+	{#each $Launches.data?.launches as launch}
+		<h1>{launch?.mission_name}</h1>
+	{/each}
+{/if}
